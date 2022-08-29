126952235815f72151cad5b8fd56fe0f3b48f716
Keith Alcock <github@keithalcock.com>
2022-08-29 10:19:51 -0700

    Auto adjust k

--- habitus_ui_interface-main/backend/surdeanu2005.py
+++ habitus_ui_interface-main/backend/surdeanu2005.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from cluster_generator import ClusterGenerator
 from corpus import Corpus
 from document import Document
@@ -23,13 +21,17 @@ class Surdeanu2005(ClusterGenerator):
 		sorted_qualities = quality_scores(clusters, siblings, documents, self.corpus.doc_distances)
 		return sorted_qualities
 
-	def generate(self, documents: list[Document], k: float, frozen_document_lists: list[list[Document]] = [],
+	def generate(self, documents: list[Document], k: int, frozen_document_lists: list[list[Document]] = [],
 			seeded_document_lists: list[list[Document]] = []) -> tuple[list[list[int]], int]:
 		sorted_qualities = self.generate_clusters(documents) # This seems to be cluster_count x document_count
 		modified_clusters = frozen_document_lists + seeded_document_lists
 		if k is None:
 			_, _, categories = get_best_initial_model(documents, sorted_qualities, self.quality_names, modified_clusters = modified_clusters)
 		else:
+			if len(modified_clusters) > k:
+				k += 2 # If you have more clusters than k, just add a couple more each time.
+			else:
+				k -= len(modified_clusters) # Always keep the right number
 			_, _, categories = get_best_initial_model_k(k, documents, self.corpus.doc_distances, sorted_qualities, self.quality_names, allowed_seed_size = self.allowed_seed_size, modified_clusters = modified_clusters)
 		# Frozen and seeded clusters are going to be the first N here in this list.
 	  # ... Remove frozen clusters because they shouldn't be added to.
