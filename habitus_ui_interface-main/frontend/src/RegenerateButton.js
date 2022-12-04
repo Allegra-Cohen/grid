53c173c8ac0d4aa8b8cc6cd625e97e8ac6a742ec
Allegra-Cohen <allegra.ab.cohen@gmail.com>
2022-12-04 13:14:31 -0500

    Fixed so that regenerate button greys out for exactly as long as it takes to load the updated Grid

--- habitus_ui_interface-main/frontend/src/RegenerateButton.js
+++ habitus_ui_interface-main/frontend/src/RegenerateButton.js
@@ -7,22 +7,17 @@ export default function RegenerateButton({onClick, apiUrl, userID}){
     useEffect(() => { setDisabled(false)}, [false] )
 
 
-    function onLaunchClicked(evt){
-        evt.preventDefault();
-        setDisabled(true);
-        setTimeout(() => setDisabled(false), 4500);
-
-    }
-
-
     return(
     <div className={"RegenerateButton"}>
     <button style={{height:'2.5em', width:'8em', background:'#90c5e1', fontSize:'20px', fontFamily: "InaiMathi"}}
-    onClick={ (evt) => {fetch(`${apiUrl}/regenerate/${userID}`)
+    onClick={ (evt) => {
+        setDisabled(true);
+        fetch(`${apiUrl}/regenerate/${userID}`)
                         .then( response => response.json())
                         .then( response => {onClick(response);
+                                            setDisabled(false);
                                             });
-                        onLaunchClicked(evt);
+                        
                }
            }
                disabled={disabled}
