864a709b099ace0a00626fd86822141332025c56
Keith Alcock <github@keithalcock.com>
2023-04-12 21:16:40 -0700

    Add beliefs UI

---
+++ habitus_ui_interface-main/frontend/src/ChangeBeliefs.js
@@ -0,0 +1,90 @@
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import {useId, useEffect, useState} from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
+import {toHex} from "./toHex"
+import './info.css';
+import './Spinner.css';
+
+
+export default function ChangeBeliefs({apiurl}) {
+	const [filepath, setFilepath] = useState([]);
+	const [waiting, setWaiting] = useState(false);
+	const [feedback, setFeedback] = useState(false);
+	const [error, setError] = useState(false);
+	const [beliefsFile, setBeliefsFile] = useState([]);
+
+	const handleInput = (text) => {
+		setFeedback(false);
+		setFilepath(text);
+	}
+
+    const handleButton = () => {
+    	if (filepath.length > 0){
+			setWaiting(true);
+			setFeedback(false);
+			console.log('eee')
+			fetch(`${apiurl}/processBeliefs/${toHex(filepath)}/`)
+				.then(response => response.json())
+	            .then(data => {
+	            	setWaiting(false);
+	            	setError(!data.success);
+	            	if (data.success){
+	            		setBeliefsFile(data.beliefs_file)
+	            	} else {
+	            		setBeliefsFile([])
+	            	};
+					setFeedback(true);
+	            });
+    	}
+    }
+
+	return (
+        <DndProvider backend={HTML5Backend}>
+			<div style={{background: "#a9d3ff", padding:"1%"}}><Link style={{color:'black'}} to="/">Back to Gallery</Link></div>
+			<div style={{marginTop:'5%'}}>
+				<div style={{display: "flex", flexDirection: "row", justifyContent:'center'}}>
+					<div className='info' style={{width:'max-content', fontSize:'14pt', }} onKeyUp={(evt) => handleInput(evt.target.value)}>
+						Please specify the beliefs file: <br/><br/> <input placeholder = "Beliefs file" style={{width:'100%',  fontSize:'14pt'}}/>
+					</div>
+					{filepath.length > 0 ? <div/> : <div style={{margin: '0.5%', padding: '1%', color: 'blue'}}>Please provide a path</div>}
+				</div>
+			</div>
+			<div style={{textAlign:'center'}}>
+				<button style={{width:'max-content',fontSize:'14pt', padding:'0.5%', backgroundColor:'#54f07d'}}
+					onClick={
+						(evt)=>
+							handleButton()
+					}
+				>
+					Ready!
+				</button>
+			</div>
+			<div>
+				{waiting ? 
+					<div>
+						<div style={{textAlign: 'center', marginTop:'2%', marginBottom:'1%'}}>
+							Preparing beliefs...  This step can take a long time.
+						</div>
+						<div className='spinner'></div>
+					</div>
+					:
+					feedback ?
+						error ?
+							<div style={{textAlign:'center', padding: '1%', color: 'red'}}>
+								Cannot process {filepath}.
+							</div>
+							:
+							<div style={{marginLeft: '-8%', margin: '0.5%', padding: '1%', textAlign:'center'}}>
+								All done! Your beliefs are ready to be used.<br/>
+								The beliefs file is <b>{beliefsFile}</b>.
+							</div>
+						
+						:
+						<div/>
+				}
+			</div>
+        </DndProvider>
+    );
+}
--- habitus_ui_interface-main/frontend/src/Gallery.js
+++ habitus_ui_interface-main/frontend/src/Gallery.js
@@ -18,8 +18,15 @@ function GridIcon({gridName, handleGridClick, apiurl}) {
         })
     })
 
-    return <Link to="/grid" className='gallery' ref={dragRef} style={{'color' : '#060e4e', 'fontSize':'14pt'}} onClick={()=>handleGridClick(gridName)}>{gridName}{isDragging}</Link>
-
+    return (
+        <Link to="/grid" className='gallery' ref={dragRef} style={{'color' : '#060e4e', 'fontSize':'14pt'}}
+            onClick={
+                ()=>
+                    handleGridClick(gridName)
+            }>
+            {gridName}{isDragging}
+        </Link>
+    );
 }
 
 function Gallery({apiurl}) {
@@ -45,36 +52,42 @@ function Gallery({apiurl}) {
 
 
 	return (
-      <DndProvider backend={HTML5Backend}>
-      <div style={{background: "#a9d3ff", padding:"1.5%"}}></div>
-      <h1 style={{marginLeft:"5%", color:'#060e4e'}}>Grids:</h1>
-      <div>
-      <ul style={{columns:numCols, marginLeft:"3.5%"}}>
-      {items}
+        <DndProvider backend={HTML5Backend}>
+            <div style={{background: "#a9d3ff", padding:"1.5%"}}></div>
+            <h1 style={{marginLeft:"5%", color:'#060e4e'}}>Grids:</h1>
+            <div>
+                <ul style={{columns:numCols, marginLeft:"3.5%"}}>
+                    {items}
+                </ul>
+            </div>
 
-      </ul>
-      </div>
-
-      <div style={{display:'flex', flexDirection:'row', marginLeft:'6%'}}>
-      <div className = 'info' style={{width:'max-content'}}><Link to="/create" style={{fontSize:'14pt', color:'#060e4e', backgroundColor: '#f0f7fd'}}> Create new Grid! </Link></div>
-        <div style={{marginLeft:'42%', marginTop:'1%'}}>
-        <Trash className='Trash'
-        onDrop={
-        (evt) => {
-            fetch(`${apiurl}/showGrids/`)
-            .then( response => response.json())
-            .then( data => {
-                setGrids(data.grids);
-                setNumCols(grids.length === 1 ? '1':'2')});
-                }
-       }
-       apiurl={apiurl}/>
-      </div>
-      </div>
-      <div className = 'info' style={{width:'max-content', marginLeft:'6%'}}><Link to="/changeCorpus" style={{fontSize:'14pt', color:'#060e4e', backgroundColor: '#f0f7fd'}}> Upload or update corpus </Link></div>
-
-      </DndProvider>
-  );
+            <div style={{display:'flex', flexDirection:'row', marginLeft:'6%'}}>
+                <div className = 'info' style={{width:'max-content'}}>
+                    <Link to="/create" style={{fontSize:'14pt', color:'#060e4e', backgroundColor: '#f0f7fd'}}> Create new Grid! </Link>
+                </div>
+                <div style={{marginLeft:'42%', marginTop:'1%'}}>
+                    <Trash className='Trash'
+                        onDrop={
+                            (evt) => {
+                                fetch(`${apiurl}/showGrids/`)
+                                .then( response => response.json())
+                                .then( data => {
+                                    setGrids(data.grids);
+                                    setNumCols(grids.length === 1 ? '1':'2')});
+                                    }
+                            }
+                        apiurl={apiurl}
+                    />
+                </div>
+            </div>
+            <div className = 'info' style={{width:'max-content', marginLeft:'6%'}}>
+                <Link to="/changeCorpus" style={{fontSize:'14pt', color:'#060e4e', backgroundColor: '#f0f7fd'}}> Upload or update corpus </Link>
+            </div>
+            <div className = 'info' style={{width:'max-content', marginLeft:'6%'}}>
+                <Link to="/changeBeliefs" style={{fontSize:'14pt', color:'#060e4e', backgroundColor: '#f0f7fd'}}> Update beliefs </Link>
+            </div>
+        </DndProvider>
+    );
 }
 
 export default Gallery;
