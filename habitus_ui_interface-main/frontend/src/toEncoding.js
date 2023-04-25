499d4604560704527828f0159a32436d55de30a6
Keith Alcock <github@keithalcock.com>
2023-04-25 16:53:21 -0700

    Use const when possible

--- habitus_ui_interface-main/frontend/src/toEncoding.js
+++ habitus_ui_interface-main/frontend/src/toEncoding.js
@@ -1,20 +1,20 @@
 export function toHex(string) {
-    var array = [];
-    for (var i = 0; i < string.length; i++)
+    const array = [];
+    for (let i = 0; i < string.length; i++)
         array[i] = ("000" + string.charCodeAt(i).toString(16)).slice(-4);
     return array.join("");
 }
 
 export function toQuery(nameAndValuePairs) {
-    let urlSearchParams = new URLSearchParams();
+    const urlSearchParams = new URLSearchParams();
     nameAndValuePairs.forEach(nameAndValue => urlSearchParams.append(nameAndValue[0], nameAndValue[1]));
-    let query = urlSearchParams.toString()
-    return query;
+    const query = urlSearchParams.toString()
+    return query.length === 0 ? "" : ("?" + query);
 }
 
 export function toRequest(apiurl, command, nameAndValuePairs) {
-    let query = toQuery(nameAndValuePairs)
-    const request = `${apiurl}/${command}/?${query}`;
+    const query = toQuery(nameAndValuePairs)
+    const request = `${apiurl}/${command}/${query}`;
     console.log("request:", request);
     return request;
 }
