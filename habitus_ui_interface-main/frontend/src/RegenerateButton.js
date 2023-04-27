82676d51fd4e5d78a7e677136b9163ecab9a5523
Keith Alcock <github@keithalcock.com>
2023-04-27 16:20:31 -0700

    RegenerateButton

--- habitus_ui_interface-main/frontend/src/RegenerateButton.js
+++ habitus_ui_interface-main/frontend/src/RegenerateButton.js
@@ -1,29 +1,31 @@
-import Backend from "./Backend"
+import Backend from "./Backend";
+import Callback from "./Callback";
 
 import {useEffect, useState} from "react";
 
-export default function RegenerateButton({onClick, apiurl}){
+import "./RegenerateButton.css";
+
+export default function RegenerateButton({onClick, apiurl}) {
     const [disabled, setDisabled] = useState([]);
 
     const backend = new Backend(apiurl);
 
-    useEffect(() => { setDisabled(false)}, [] )
+    useEffect(() => {
+        setDisabled(false);
+    }, []);
+
+    const handleButtonClick = new Callback("RegenerateButton.handleButtonClick").get(evt => {
+        setDisabled(true);
+        const request = backend.toRequest("regenerate")
+        backend.fetch(request, response => {
+            onClick(response);
+            setDisabled(false);
+        });
+    });
 
     return (
         <div className={"RegenerateButton"}>
-            <button style={{height:'2.5em', width:'8em', background:'#90c5e1', fontSize:'20px', fontFamily: "InaiMathi"}}
-                onClick={(evt) => {
-                    setDisabled(true);
-                    const request = backend.toRequest("regenerate")
-                    backend.fetch(request, response => {
-                        onClick(response);
-                        setDisabled(false);
-                    });                        
-                }}
-                disabled={disabled}
-            >
-                Update Grid
-            </button>
+            <button onClick={handleButtonClick} disabled={disabled}>Update Grid</button>
         </div>
     );
 }
